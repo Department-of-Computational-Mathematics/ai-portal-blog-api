154f4181f38ac78f74bc442bbaa41c1c1cb0d8cd
Ravindu Weerasinghe <ravindutharuka2001@gmail.com>
2025-06-24 10:06:40 +0530

    Updated the schema to consider user_id optional
    user_id will be set on the server side hence it will not be required for the request body

--- app/schemas/blog.py
+++ app/schemas/blog.py
@@ -1,12 +1,12 @@
   
 from pydantic import BaseModel, Field
 from uuid import uuid4
-from typing import List
+from typing import List, Optional
 from datetime import datetime
 
 class BlogPost(BaseModel): #represents a single blog post
     blogPost_id: str = Field(default_factory=lambda: str(uuid4()), alias="_id")    #blogPost_id:UUID = Field(default_factory=uuid4, alias="_id") #primary key
-    user_id:str   #UUID to str ,author`s user_id
+    user_id: Optional[str] = None   #UUID to str ,author`s user_id - Optional for request, set server-side
     comment_constraint:bool #whether commenting is enabled or not, if enabled then true
     tags:List[int]  # List of integers indicating relavant tags (topics)
     number_of_views:int
@@ -16,7 +16,7 @@ class BlogPost(BaseModel): #represents a single blog post
 
 class Comment(BaseModel):
     comment_id: str = Field(default_factory=lambda: str(uuid4()), alias="_id")#UUID = Field(default_factory=uuid4, alias="_id") #primary key
-    user_id:str #UUID to str ,commenting person`s user_id
+    user_id: Optional[str] = None #UUID to str ,commenting person`s user_id - Optional for request, set server-side
     blogPost_id:str #UUID to str 
     text:str #comment content
     commentedAt:datetime = Field(default_factory=datetime.utcnow) #commented date time -utc time
@@ -25,7 +25,7 @@ class Comment(BaseModel):
 class Reply(BaseModel):
     reply_id: str = Field(default_factory=lambda: str(uuid4()), alias="_id")#UUID = Field(default_factory=uuid4, alias="_id") #primary key
     parentContent_id:str #UUID to str ,either a comment_id or reply_id (when someone reply to an existing reply)
-    user_id:str #UUID t str, replying person`s` user_id
+    user_id: Optional[str] = None #UUID to str, replying person`s` user_id - Optional for request, set server-side
     text:str #reply content
     repliedAt:datetime = Field(default_factory=datetime.utcnow) #replied date time -utc time
     replies: List['Reply'] = []  #for fetch_comments_and_replies() function

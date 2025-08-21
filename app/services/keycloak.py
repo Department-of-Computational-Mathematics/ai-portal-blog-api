25fee4dbd992e3ff2228432ca6d6deeb8eb087ee
Ravindu Weerasinghe <ravindutharuka2001@gmail.com>
2025-08-21 20:20:44 +0530

    Refactor Keycloak service to use KeycloakUser schema for user data handling

--- app/services/keycloak.py
+++ app/services/keycloak.py
@@ -4,7 +4,7 @@ from typing import List, Dict, Optional
 from pprint import pprint
 
 from app.core.config import settings
-
+from app.schemas.blog import KeycloakUser
 
 def get_keycloak_token() -> Optional[str]:
     resp = requests.post(
@@ -25,7 +25,7 @@ def get_keycloak_token() -> Optional[str]:
         )
 
 
-def get_all_users() -> List[Dict]:
+def get_all_users() -> List[KeycloakUser]:
     token = get_keycloak_token()
     if not token:
         raise HTTPException(
@@ -38,14 +38,14 @@ def get_all_users() -> List[Dict]:
         timeout=10,
     )
     if resp.status_code == 200:
-        return resp.json()
+        return [KeycloakUser(**user) for user in resp.json()]
     raise HTTPException(
         status_code=500,
         detail=f"Internal Server Error. Keycloak returned a {resp.status_code} - {resp.text} error"
     )
 
 
-def get_user_by_id(user_id: str) -> Optional[Dict]:
+def get_user_by_id(user_id: str) -> KeycloakUser:
     token = get_keycloak_token()
     if not token:
         raise HTTPException(
@@ -58,7 +58,8 @@ def get_user_by_id(user_id: str) -> Optional[Dict]:
         timeout=10,
     )
     if resp.status_code == 200:
-        return resp.json()
+        data = resp.json()
+        return KeycloakUser(**data)
     if resp.status_code == 404:
         raise HTTPException(
             status_code=404,

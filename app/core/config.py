57702ecb64665352170355dfd8a09f179c5b217e
Ravindu Weerasinghe <ravindutharuka2001@gmail.com>
2025-08-21 23:12:29 +0530

    Remove sensitive info from codebase and move them to env vars

--- app/core/config.py
+++ app/core/config.py
@@ -14,14 +14,14 @@ class Settings(BaseSettings):
     DATABASE_URL: str = "sqlite:///./blog.db"  # Update this based on your database configuration
     
     # MongoDB settings
-    MONGODB_URL: str = "mongodb+srv://AIPortalBlogAdmin:3F45Tohxct3jb2Ih@email.vm8njwj.mongodb.net/"
-    MONGODB_DB_NAME: str = "AIPortalBlog"
-    
+    MONGODB_URL: str = os.getenv("BLOG_MONGODB_URL", "mongodb://localhost:27017")
+    MONGODB_DB_NAME: str = os.getenv("BLOG_MONGODB_DB_NAME", "")
+
     # Keycloak settings
     KEYCLOAK_URL: str = "http://localhost:8080"
     REALM: str = "master"
     CLIENT_ID: str = "blogs-service"  # Custom client used for blog service (separation of concerns)
-    CLIENT_SECRET: str = os.getenv("BLOGS_CLIENT_SECRET", "")
+    CLIENT_SECRET: str = os.getenv("BLOG_CLIENT_SECRET", "")
 
     class Config:
         case_sensitive = True
